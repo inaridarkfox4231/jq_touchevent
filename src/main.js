2eabd4f7df5bb0dc619d3450432a5aa92a6a89dd
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-08-07 21:41:02 +0900

    座標を表示

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 $(function(){
   var box = $('#touchBox')[0];
   box.addEventListener("touchstart", touchHandler, false);
+  box.addEventListener("touchmove", touchHandler, false);
+  box.addEventListener("touchend", touchHandler, false);
   function touchHandler(e){
-    alert("Touch!!!");
+    e.preventDefault();  // デフォルトのイベント（コピーなど）を防ぐ
+    if(e.type == "touchstart"){
+      $('#txt').text("タッチされました");
+    }
+    if(e.type == "touchmove"){
+      $('#txt').text("フリックしています");
+      $('#x_pos').text(e.pageX);
+      $('#y_pos').text(e.pageY);
+    }
+    if(e.type == "touchend"){
+      $('#txt').text("フリックが終わりました");
+    }
   }
 })
